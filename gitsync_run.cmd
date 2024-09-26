c54eb90cc789f8c9c3989601621a66135611db6b
NadiGri <nadi.grishchenko@gmail.com>
2024-09-27 00:31:32 +0300

    Tasc-3
    сохранение скриптов

--- gitsync_run.cmd
+++ gitsync_run.cmd
@@ -1,2 +1,10 @@
 ## Инициализация гитсинха
 gitsync init -u gitbot e:\OTUS\Storage\ e:\OTUS\L7_gitsunc\src\cf\
+
+## Синхронизация с хранилищем
+
+gitsync sync -u gitbot e:\OTUS\Storage\ e:\OTUS\L7_gitsunc\src\cf\
+
+## формирование релиза
+
+vrunner compile --src src/cf --out build/1cv8.cf
